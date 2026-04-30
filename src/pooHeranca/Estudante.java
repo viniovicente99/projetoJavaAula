05a5e2066b65889b21730db03c8599f3cc4a8a22
viniovicente99 <viniovicente99@gmail.com>
2026-04-30 10:22:53 -0300

    POO - Herança

--- src/pooHeranca/Estudante.java
+++ src/pooHeranca/Estudante.java
@@ -1,39 +1,17 @@
 package pooHeranca;
 
-public class Estudante {
+public class Estudante extends Pessoa {
 	
-	private String nome;
-    private int idade;
-    private String email;
     private String matricula;
     private String curso;
     
 	public Estudante(String nome, int idade, String email, String matricula, String curso) {
-		this.nome = nome;
-		this.idade = idade;
-		this.email = email;
+		super(nome, idade, email);
 		this.matricula = matricula;
 		this.curso = curso;
+		
 	}
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
 	public String getMatricula() {
 		return matricula;
 	}
